018770fb233648d15feec860ed916559f290c7eb
RukShu <arturoaureliohr@gmail.com>
2022-09-24 22:13:27 -0400

    Remove eslint disable comment in Base Entity file

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,31 +1,35 @@
-/* eslint-disable prettier/prettier */
-import { CreateDateColumn, DeleteDateColumn, PrimaryColumn, UpdateDateColumn} from "typeorm";
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class BaseEntity {
-	@PrimaryColumn({name: 'id', nullable: false, type: 'uuid'})
-	id: string;
+  @PrimaryColumn({ name: 'id', nullable: false, type: 'uuid' })
+  id: string;
 
-	@CreateDateColumn({
-		name: 'created_at',
-		nullable: false,
-		type: 'timestamp',
-		default: 'now()',
-	})
-	createdAt: Date;
+  @CreateDateColumn({
+    name: 'created_at',
+    nullable: false,
+    type: 'timestamp',
+    default: 'now()',
+  })
+  createdAt: Date;
 
-	@DeleteDateColumn({
-		name: 'deleted_at',
-		nullable: true,
-		type: 'timestamp',
-		default: null,
-	})
-	deletedAt: Date;
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    nullable: true,
+    type: 'timestamp',
+    default: null,
+  })
+  deletedAt: Date;
 
-	@UpdateDateColumn({
-		name: 'updated_at',
-		nullable: true,
-		type: 'timestamp',
-		default: 'now',
-	})
-	updatedAt: Date;
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: true,
+    type: 'timestamp',
+    default: 'now',
+  })
+  updatedAt: Date;
 }

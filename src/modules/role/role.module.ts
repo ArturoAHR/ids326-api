63f6d3bd9828844a648a10bf478895b1e9bd647e
RukShu <arturoaureliohr@gmail.com>
2022-09-25 15:20:15 -0400

    Import Typeorm Repositories in Role Module

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
+import { RoleRepository } from './role.repository';
+import { Role } from '../../entities/role.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Role, RoleRepository])],
   providers: [RoleService],
-  controllers: [RoleController]
+  controllers: [RoleController],
 })
 export class RoleModule {}

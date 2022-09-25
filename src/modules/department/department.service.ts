90780b240670aa2029a8f266e7c806f9f51c8a90
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:21:04 -0400

    Add getAll method in Department Service

--- src/modules/department/department.service.ts
+++ src/modules/department/department.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Department } from '../../entities/department.entity';
+import { DepartmentRepository } from './department.repository';
 
 @Injectable()
-export class DepartmentService {}
+export class DepartmentService {
+  constructor(private readonly departmentRepository: DepartmentRepository) {}
+
+  getAll = async (): Promise<Department[]> => {
+    const companies = await this.departmentRepository.getAll();
+    return companies;
+  };
+}

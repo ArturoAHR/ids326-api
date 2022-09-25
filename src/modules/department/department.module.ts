dc647e6f5c9de054a2dd3983d8b7f6bc9b6e117c
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:19:54 -0400

    Implement Typeorm Repositories in Department Module

--- src/modules/department/department.module.ts
+++ src/modules/department/department.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DepartmentService } from './department.service';
 import { DepartmentController } from './department.controller';
+import { DepartmentRepository } from './department.repository';
+import { Department } from '../../entities/department.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Department, DepartmentRepository])],
   providers: [DepartmentService],
-  controllers: [DepartmentController]
+  controllers: [DepartmentController],
 })
 export class DepartmentModule {}

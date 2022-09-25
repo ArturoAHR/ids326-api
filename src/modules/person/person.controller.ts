f90505c55bedcfbaf63f668a627630ccfa143c61
RukShu <arturoaureliohr@gmail.com>
2022-09-25 12:50:32 -0400

    Implement Person Service getAll method in Person Controller

--- src/modules/person/person.controller.ts
+++ src/modules/person/person.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Person } from '../../entities/person.entity';
+import { PersonService } from './person.service';
 
 @Controller('person')
-export class PersonController {}
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  @Get()
+  async getAll(): Promise<Person[]> {
+    return await this.personService.getAll();
+  }
+}

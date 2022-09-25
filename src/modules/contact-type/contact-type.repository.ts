2b56af9b185f7c03040f12120ebfc7eed49b4ee2
RukShu <arturoaureliohr@gmail.com>
2022-09-25 15:47:22 -0400

    Implement Get methods in Contact Type Repository

--- src/modules/contact-type/contact-type.repository.ts
+++ src/modules/contact-type/contact-type.repository.ts
@@ -2,4 +2,12 @@ import { EntityRepository, Repository } from 'typeorm';
 import { ContactType } from '../../entities/contact-type.entity';
 
 @EntityRepository()
-export class ContactTypeRepository extends Repository<ContactType> {}
+export class ContactTypeRepository extends Repository<ContactType> {
+  async getAll(): Promise<ContactType[]> {
+    return await this.createQueryBuilder('contactType').getMany();
+  }
+
+  async getById(id: string): Promise<ContactType> {
+    return await this.findOneOrFail({ id: id });
+  }
+}

fb456dfddf83d86f3c39532d831c5bdfe847f9bb
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:21:59 -0400

    Add Department Get All endpoint to Department Controller

--- src/modules/department/department.controller.ts
+++ src/modules/department/department.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Department } from '../../entities/department.entity';
+import { DepartmentService } from './department.service';
 
 @Controller('department')
-export class DepartmentController {}
+export class DepartmentController {
+  constructor(private readonly departmentService: DepartmentService) {}
+
+  @Get()
+  async getAll(): Promise<Department[]> {
+    return await this.departmentService.getAll();
+  }
+}

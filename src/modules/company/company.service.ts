64d8dd493dce3d2f51077ad69c32fefef36fa07a
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:04:31 -0400

    Add getAll method to Company Service

--- src/modules/company/company.service.ts
+++ src/modules/company/company.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Company } from '../../entities/company.entity';
+import { CompanyRepository } from './company.repository';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(private readonly companyRepository: CompanyRepository) {}
+
+  getAll = async (): Promise<Company[]> => {
+    const companies = await this.companyRepository.getAll();
+    return companies;
+  };
+}

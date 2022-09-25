b7a67a09b6f8eef4dfc10c881a9b9d038f340503
RukShu <arturoaureliohr@gmail.com>
2022-09-25 15:30:12 -0400

    Implement CRUD methods in Role Service

--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { DeleteResult } from 'typeorm';
+import { CreateRoleRequestDTO } from '../../dto/role/create-role-request.dto';
+import { UpdateRoleRequestDTO } from '../../dto/role/update-role-request.dto';
+import { Role } from '../../entities/role.entity';
+import { RoleRepository } from './role.repository';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(private readonly roleRepository: RoleRepository) {}
+
+  getAll = async (): Promise<Role[]> => {
+    const roles = await this.roleRepository.getAll();
+    return roles;
+  };
+
+  create = async (dto: CreateRoleRequestDTO): Promise<Role> => {
+    const newRole: Partial<Role> = {
+      name: dto.name,
+      description: dto.description,
+    };
+    const savedRole = await this.roleRepository.save(newRole);
+    return savedRole;
+  };
+
+  getById = async (id: string): Promise<Role> => {
+    const role = await this.roleRepository.getById(id);
+    return role;
+  };
+
+  update = async (id: string, dto: UpdateRoleRequestDTO): Promise<Role> => {
+    const newRole: Partial<Role> = {
+      id,
+      name: dto.name,
+      description: dto.description,
+    };
+    const savedRole = await this.roleRepository.save(newRole);
+    return savedRole;
+  };
+
+  delete = async (id: string): Promise<boolean> => {
+    const deleteResult: DeleteResult = await this.roleRepository.delete(id);
+    return deleteResult.affected > 0;
+  };
+}

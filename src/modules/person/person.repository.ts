21687cd1b0e420588ebfc8cd58e4f6b193b69bf1
RukShu <arturoaureliohr@gmail.com>
2022-09-25 12:50:03 -0400

    Add get all method to Person Repository

--- src/modules/person/person.repository.ts
+++ src/modules/person/person.repository.ts
@@ -2,4 +2,8 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Person } from '../../entities/person.entity';
 
 @EntityRepository(Person)
-export class PersonRepository extends Repository<Person> {}
+export class PersonRepository extends Repository<Person> {
+  async getAll(): Promise<Person[]> {
+    return await this.createQueryBuilder('person').getMany();
+  }
+}

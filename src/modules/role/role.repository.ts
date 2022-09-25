a3a44c02f93db43d426c85942c7b122188cbd1fb
RukShu <arturoaureliohr@gmail.com>
2022-09-25 15:19:31 -0400

    Implement Repository Get Role methods

--- src/modules/role/role.repository.ts
+++ src/modules/role/role.repository.ts
@@ -2,4 +2,12 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Role } from '../../entities/role.entity';
 
 @EntityRepository()
-export class RoleRepository extends Repository<Role> {}
+export class RoleRepository extends Repository<Role> {
+  async getAll(): Promise<Role[]> {
+    return await this.createQueryBuilder('role').getMany();
+  }
+
+  async getById(id: string): Promise<Role> {
+    return await this.findOneOrFail({ id: id });
+  }
+}

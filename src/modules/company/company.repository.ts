f6ae73d978907691bed92ed0dd382092f28e5939
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:02:39 -0400

    Add getAll method to Company Repository

--- src/modules/company/company.repository.ts
+++ src/modules/company/company.repository.ts
@@ -2,4 +2,8 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Company } from '../../entities/company.entity';
 
 @EntityRepository()
-export class CompanyRepository extends Repository<Company> {}
+export class CompanyRepository extends Repository<Company> {
+  async getAll(): Promise<Company[]> {
+    return await this.createQueryBuilder('company').getMany();
+  }
+}

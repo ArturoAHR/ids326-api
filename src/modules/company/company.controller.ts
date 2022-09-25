8fd1ae244ebec034cfdab29387d76c07dd5b750d
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:06:52 -0400

    Add endpoint to get all companies

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Company } from '../../entities/company.entity';
+import { CompanyService } from './company.service';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+  constructor(private readonly companyService: CompanyService) {}
+
+  @Get()
+  async getAll(): Promise<Company[]> {
+    return await this.companyService.getAll();
+  }
+}

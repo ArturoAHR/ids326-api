78f5530aff1ab9257374bf42f5c1ecbc657a2a8d
RukShu <arturoaureliohr@gmail.com>
2022-09-24 22:42:47 -0400

    Add company entity class

---
+++ src/entities/company.entity.ts
@@ -0,0 +1,30 @@
+import { Column, JoinColumn, OneToOne } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Person } from './person.entity';
+
+export class Company extends BaseEntity {
+  @Column({ name: 'name', type: 'varchar' })
+  name: string;
+
+  @Column({ name: 'ceo_id', type: 'uuid', nullable: true })
+  ceoId: string;
+
+  @Column({ name: 'primary_phone', type: 'varchar' })
+  primaryPhone: string;
+
+  @Column({ name: 'secondary_phone', type: 'varchar', nullable: true })
+  secondaryPhone: string;
+
+  @Column({ name: 'fax', type: 'varchar' })
+  fax: string;
+
+  @Column({ name: 'email', type: 'varchar', nullable: true })
+  email: string;
+
+  @Column({ name: 'website', type: 'varchar', nullable: true })
+  website: string;
+
+  @OneToOne(() => Person, { eager: true })
+  @JoinColumn({ name: 'ceo_id' })
+  ceo: Promise<Person>;
+}

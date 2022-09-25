4b400fd2ba7d94b55bb973a314f7e53e413abee2
RukShu <arturoaureliohr@gmail.com>
2022-09-25 15:44:04 -0400

    Import Typeorm Repository and Entity in Contact Type Module

--- src/modules/contact-type/contact-type.module.ts
+++ src/modules/contact-type/contact-type.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactType } from '../../entities/contact-type.entity';
 import { ContactTypeController } from './contact-type.controller';
+import { ContactTypeRepository } from './contact-type.repository';
 import { ContactTypeService } from './contact-type.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ContactType, ContactTypeRepository])],
   providers: [ContactTypeService],
   controllers: [ContactTypeController],
 })

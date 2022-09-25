02bcfe027d04eb9b0f749f779b3534ae62248a98
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:01:31 -0400

    Implement Typeorm Repositories in Company Module

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from '../../entities/company.entity';
 import { CompanyController } from './company.controller';
+import { CompanyRepository } from './company.repository';
 import { CompanyService } from './company.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Company, CompanyRepository])],
   controllers: [CompanyController],
   providers: [CompanyService],
 })

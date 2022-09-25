1a3670f9c4fc6c8aacfd39109eb48f1795cf16dc
RukShu <arturoaureliohr@gmail.com>
2022-09-25 12:50:16 -0400

    Implement Get All method in Person Service

--- src/modules/person/person.service.ts
+++ src/modules/person/person.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Person } from '../../entities/person.entity';
+import { PersonRepository } from './person.repository';
 
 @Injectable()
-export class PersonService {}
+export class PersonService {
+  constructor(private readonly personRepository: PersonRepository) {}
+
+  getAll = async (): Promise<Person[]> => {
+    return this.personRepository.getAll();
+  };
+}

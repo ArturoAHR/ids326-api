ce7ee2584728655f01e5e9d34b67e4ea61578bf3
RukShu <arturoaureliohr@gmail.com>
2022-09-25 00:08:29 -0400

    Add Departments factory

---
+++ src/database/factories/department.factory.ts
@@ -0,0 +1,40 @@
+import { Department } from '../../entities/department.entity';
+
+export const DEPARTMENTS: Department[] = [
+  {
+    id: 'fd543737-7910-47c2-8aac-ab94ccf4801a',
+    name: 'Accountability',
+    description: 'Accountability Department',
+    code: 'AC',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    id: '69d5070f-28f4-4d81-8fda-1a5ee2209eb2',
+    name: 'Software Development',
+    description: 'Software Development Department',
+    code: 'SD',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    id: 'f844f9f6-bb75-440b-96eb-27f3b8ec6fc8',
+    name: 'Quality Assurance',
+    description: 'Quality Assurance Department',
+    code: 'QA',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    id: '6587b126-9faf-4b4d-8bb0-750d1f225769',
+    name: 'Human Resources',
+    description: 'Human Resources Department',
+    code: 'HR',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+];

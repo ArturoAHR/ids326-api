61c26375db98e853c2b8114530b80510f3988b18
RukShu <arturoaureliohr@gmail.com>
2022-09-25 16:18:46 -0400

    Add getAll method to Department repository

--- src/modules/department/department.repository.ts
+++ src/modules/department/department.repository.ts
@@ -2,4 +2,8 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Department } from '../../entities/department.entity';
 
 @EntityRepository()
-export class ContactTypeRepository extends Repository<Department> {}
+export class DepartmentRepository extends Repository<Department> {
+  async getAll(): Promise<Department[]> {
+    return await this.createQueryBuilder('department').getMany();
+  }
+}
